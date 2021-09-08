526a4b6a7b99e8dd7f39eeca40ea402326c8e758
parkerschwantes <23pschwantes@athenian.org>
2021-09-08 12:23:51 -0700

    not quite complete

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,20 +4,54 @@ public class MyMain {
     
     // Counts the number of 'a', 'b', and 'c' chars in str
     public static int countABC(String str) {
-        // REPLACE WITH YOUR CODE
+        int counterABC = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.indexOf("a") != -1) {
+                counterABC = counterABC + 1;
+            }
+            else if (str.indexOf("c") != -1) {
+                counterABC = counterABC + 1;
+            }
+            else if (str.indexOf("b") != -1); {
+                counterABC = counterABC + 1;
+            }
+
+        }
+        System.out.println("There were " + counterABC + " A's, B's, and C's");
         return -1;
     }
 
     // Checks to see whether or not str contains 'The' or 'the'
     public static boolean containsThe(String str) {
-        // REPLACE WITH YOUR CODE
-        return false;
-    }
+        int y = str.indexOf("The");
+        int x = str.indexOf("the");
+        if (y == -1 || x == -1) {
+            return false;
+        }
+        else {
+            return true;
+            }
+
+        }
+
+
+
 
     // Checks whether str is a palindrome or not
     public static boolean isPalindrome(String str) {
-        // REPLACE WITH YOUR CODEE
-        return false;
+        int len = str.length();
+        for (int i = 0; i < str.length(); i++)
+            if (str.charAt(i).equals(str.charAt(len))) {
+                len = len - 1;
+                if (len = 0) {
+                    return true;
+                }
+            }
+            else {
+                return false;
+            }
+
+
     }
     
     
